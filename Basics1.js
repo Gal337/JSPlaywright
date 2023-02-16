4246b191ddf24e00f562c586a04db8fa94572e80
gsesek <destined19876@gmail.com>
2023-02-16 10:54:04 +0100

    First commit

---
+++ Basics1.js
@@ -0,0 +1,17 @@
+/*Printing to console*/
+console.log("Hello, this are the JS basics")
+
+
+/*Declaring variables with var, let or const*/
+let a = 4
+console.log(a)
+console.log(typeof(a))
+
+let b = 234.6
+console.log(typeof(b))
+
+let c = "Javascript"
+console.log(typeof(c))
+
+let required = true
+console.log(typeof(required))
