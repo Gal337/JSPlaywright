ba4dd659523a775dce3abb3567ecbbe22a110d2c
gsesek <destined19876@gmail.com>
2023-02-16 11:40:43 +0100

    Added examples for declaring variables

--- Basics1.js
+++ Basics1.js
@@ -3,7 +3,8 @@ console.log("Hello, this are the JS basics")
 
 
 /*Declaring variables with var, let or const
-Redeclaring variable with let keyword is not possible, but we can redeclare it with keyword var*/
+Redeclaring variable with let keyword is not possible, but we can redeclare it with keyword var
+const keyword should be used for variables that are not changing their values / stay always the same*/
 let a = 4
 console.log(a)
 console.log(typeof(a))
@@ -19,6 +20,10 @@ console.log(typeof(required))
 /*Reverse operator - only for boolean type*/
 console.log(!required)
 
+/*let c = a + b (it did not work because redeclaring same variable is not possible with let keyword)
+if we type:
+c = a + b 
+will also work, because we did not use keyword let, which was previously used*/
 var c = a + b
 console.log(c)
 
