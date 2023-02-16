5ae6f38a9c186a883a4817a2c277cf337eb24d84
gsesek <destined19876@gmail.com>
2023-02-16 11:47:36 +0100

    Added Basics2 - if statement and loops

---
+++ Basics2.js
@@ -0,0 +1,12 @@
+/*Declaring constant variable with the name flag and value true*/
+const flag = true
+
+/*If condition matches; first block of code gets executed, otherwise second block of code will be executed*/
+if (flag)
+{
+console.log("Condition satisfied")  
+}
+else
+{
+console.log("Condition not satisfied")  
+}
