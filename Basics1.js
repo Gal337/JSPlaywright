604ecf494e1f27a6599320a934d72227e2d0a6ba
gsesek <destined19876@gmail.com>
2023-02-16 11:29:35 +0100

    Extended comment for declaring variables

--- Basics1.js
+++ Basics1.js
@@ -2,7 +2,8 @@
 console.log("Hello, this are the JS basics")
 
 
-/*Declaring variables with var, let or const*/
+/*Declaring variables with var, let or const
+Redeclaring variable with let keyword is not possible, but we can redeclare it with keyword var*/
 let a = 4
 console.log(a)
 console.log(typeof(a))
@@ -10,8 +11,12 @@ console.log(typeof(a))
 let b = 234.6
 console.log(typeof(b))
 
-let c = "Javascript"
+var c = "Javascript"
 console.log(typeof(c))
 
 let required = true
 console.log(typeof(required))
+
+var c = a + b
+console.log(c)
+
