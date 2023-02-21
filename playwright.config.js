e2519ad91655d5bf9149d69e0b617c9b7d900352
gsesek <destined19876@gmail.com>
2023-02-21 15:23:51 +0100

    deleted example.spec.js from project, updated cfg

--- playwright.config.js
+++ playwright.config.js
@@ -33,6 +33,8 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    /*Declare which browser to use*/
+    browserName: 'chromium'
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -89,4 +91,3 @@ module.exports = defineConfig({
   //   port: 3000,
   // },
 });
-
---
+++ tests/UIBasicsTest.spec.js
@@ -0,0 +1,25 @@
+//const {test} = require ('@playwright/test'); needs to be added so test can execute
+const {test} = require('@playwright/test');
+//async needs to be added before function so the steps will be executed in order
+//instead of function, we can also write just ()=> ; instead of browser we can also use {page} if we don't need anything else
+test('Browser Context Playwright test',async ({browser})=>
+{
+  //Creating new instance
+  const context = await browser.newContext();
+  //Creating actual page to automate
+  const page = await context.newPage();
+  //Opening specific URL
+  const link = await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+
+});
+
+//If we use page, we don't need to create context and page instances
+//Page is used when we don't need to inject cookies
+test('First Playwright test',async ({page})=>
+{
+  //Opening specific URL
+  const link = await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+
+});
+
+
