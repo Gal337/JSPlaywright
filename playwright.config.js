96560ee1cd297b82647519ec7230306465f0c13e
gsesek <destined19876@gmail.com>
2023-02-21 15:52:19 +0100

    Changed headless mode to false in config

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
   use: {
     /*Declare which browser to use*/
     browserName: 'chromium',
-    headless: true,
+    headless: false,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
