68cf9ea1d35bf90c5f3616a3cff3bd280a686111
gsesek <destined19876@gmail.com>
2023-02-21 09:48:04 +0100

    Updated Basics4 file

--- Basics4.js
+++ Basics4.js
@@ -1,6 +1,11 @@
 /*Functions*/
 /*Block of code that can be executed together by wrapping it in brackets*/
 /*Declaring the function: keyword function, name of the function and parameters */
+//KEYWORDS: var, let, const
+/*var - global level / functional - can only be used inside function*/
+/*let - global level / block level {} - only used inside a block*/
+/*const - same as let but can not be reinitialized*/
+
 function add(a,b)
 {
  return a+b
@@ -19,3 +24,14 @@ let sumOfIntegers = function(c,d)
 //Minimizing the code above
 let sumOfNumbers = (c,d) => c+d
 console.log(sumOfNumbers(2,3))
+
+
+
+
+
+
+
+
+
+
+
