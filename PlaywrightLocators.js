073aa20fe18daf332b1e3883a65b231271c18fc7
gsesek <destined19876@gmail.com>
2023-02-22 10:04:51 +0100

    File with instructions for writing locators

--- PlaywrightLocators.js
+++ PlaywrightLocators.js
@@ -1,13 +1,17 @@
 /*CREATING CSS LOCATORS*/
+/*Locator examples are based on https://rahulshettyacademy.com/loginpagePractise/ */
 
 /* If Id is present:
 css -> tagname#id or #id
+example: input#username or #username
 
 If class attribute is present:
 css -> tagname.class or .class
+example: input.form-control .form-control
 
 Creating CSS locator based on any attribute:
 css -> [attribute='value']
+example: [name='username']
 
 Write CSS with traversing from parent to child:
 css -> parentTagName >> childTagName
