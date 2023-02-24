963cde655bb7ffc24130f218e7dae0d26c9e3335
gsesek <destined19876@gmail.com>
2023-02-24 11:14:38 +0100

    Extended child window handling with string methods

--- tests/UIControls.spec.js
+++ tests/UIControls.spec.js
@@ -50,7 +50,12 @@ test.only('Child Window Handling test',async ({browser})=>
   ])
   
   const text = await newPage.locator(".red").textContent();
-
-  console.log(text);
+  //Extracting email from string and entering it to username field on parent page
+  const arrayText = text.split("@");
+  const domain = arrayText[1].split(" ")[0];
+  console.log(domain);
+  await page.locator("#username").type(domain);
+  await page.pause();
+  console.log(await page.locator("#username").textContent());
 
 });
