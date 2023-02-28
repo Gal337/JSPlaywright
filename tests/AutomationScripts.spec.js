192daa23cb6499894551b2c6b9f3bb371be50108
gsesek <destined19876@gmail.com>
2023-02-28 15:06:56 +0100

    Extended dropdown menu test to be E2E test

--- tests/AutomationScripts.spec.js
+++ tests/AutomationScripts.spec.js
@@ -62,17 +62,19 @@ test('Dynamic product search test', async ({page}) =>
 
 
 //Test for handling auto suggestive dropdown options
-test.only('Auto suggestive dropdown options test', async ({page}) =>
+//(Complete E2E flow of placing the order and grabbing the order id)
+test.only('Auto suggestive dropdown options & E2E flow test', async ({page}) =>
 {
+  const email = "4testing@gmail.com";
   const productName = 'iphone 13 pro';
   const link = "https://rahulshettyacademy.com/client/";
-  const email = page.locator("#userEmail");
+  const emailField = page.locator("#userEmail");
   const password = page.locator("#userPassword");
   const loginBtn = page.locator("[value='Login']");
   const products = page.locator(".card-body");
 
   await page.goto(link);
-  await email.fill("4testing@gmail.com");
+  await emailField.fill("4testing@gmail.com");
   await password.type("Abc!2345");
   await loginBtn.click();
   //Waiting for the website to load
@@ -97,7 +99,7 @@ test.only('Auto suggestive dropdown options test', async ({page}) =>
   await page.locator("div li").first().waitFor()
   //Finding element based on tag + text (h3:has-text(variable or "enter text here"))
   //.isVisible method returns boolean value after checking if the page is loaded (.waitFor)
-  const bool = page.locator("h3:has-text(productName)").isVisible();
+  const bool = page.locator("h3:has-text('iphone 13 pro')").isVisible();
   //Adding assertion to check if we have the selected item in cart
   expect(bool).toBeTruthy();
   //Locating the checkout button based on text; only use text locator for static elements
@@ -123,7 +125,34 @@ test.only('Auto suggestive dropdown options test', async ({page}) =>
       break;
     }
   }
-
+  //Action (.toHaveText) is performed outside of expect assertion, so await needs to be outside of expect assertion
+  await expect(page.locator(".user__name label[type='text']")).toHaveText(email);
+  //Submitting the order; clicking on Place order button
+  await page.locator(".action__submit").click();
+  //CHecking if the order was confirmed successfully
+  await expect(page.locator(".hero-primary")).toHaveText("Thankyou for the order.");
+  //Getting the order ID
+  const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+  console.log(orderId);
+  //Clicking on Orders button, to see order history
+  await page.locator("button[routerlink*='/dashboard/myorders']").click();
+  //Waiting for table to be loaded
+  await page.locator("tbody").waitFor();
+  //Saving order ID rows to variable
+  const rows = await page.locator("tbody tr");
+  for (let i = 0; i < await rows.count(); i++)
+  {
+    //Saving order IDs to variable
+    const rowOrderId = await rows.nth(i).locator("th").textContent();
+    if (orderId.includes(rowOrderId))
+    {
+      await rows.nth(i).locator("button").first().click();
+      break;
+    }
+  }
+  //Saving order details to variable
+  const orderIdDetails = await page.locator(".col-text").textContent();
+  expect(orderId.includes(orderIdDetails)).toBeTruthy()
 
   await page.pause();
 });
