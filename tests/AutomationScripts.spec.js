4ebb147e97ab2f9f0e4d64666a25ba18e53c3daf
gsesek <destined19876@gmail.com>
2023-02-28 08:41:27 +0100

    Updated automation scripts file

--- tests/AutomationScripts.spec.js
+++ tests/AutomationScripts.spec.js
@@ -9,16 +9,16 @@ test('Automated login test',async ({page})=>
   const loginBtn = page.locator("[value='Login']");
 
   await page.goto(link);
-  await email.fill("destined19876@gmail.com");
+  await email.fill("4testing@gmail.com");
   await password.type("Abc!2345");
   await loginBtn.click();
 });
 
 //Test that dynamically goes through a list of products and adds them to cart
-test('Dynamic product search test', async ({page}) =>
+test.only('Dynamic product search test', async ({page}) =>
 {
   //productName variable is being used because we want specific item to be returned
-  const productName = 'Iphone 13 Pro';
+  const productName = 'iphone 13 pro';
   const link = "https://rahulshettyacademy.com/client/";
   const email = page.locator("#userEmail");
   const password = page.locator("#userPassword");
@@ -26,7 +26,7 @@ test('Dynamic product search test', async ({page}) =>
   const products = page.locator(".card-body");
 
   await page.goto(link);
-  await email.fill("destined19876@gmail.com");
+  await email.fill("4testing@gmail.com");
   await password.type("Abc!2345");
   await loginBtn.click();
   //Waiting for the website to load
@@ -37,12 +37,13 @@ test('Dynamic product search test', async ({page}) =>
   console.log(titles);
   //Counts variable returns us the number of items with chosen selector
   const count = await products.count();
-  for(let i = 0; i < count; i++)
+  for(let i = 0; i < count; ++i)
   {
-  if (products.nth(i).locator("b").textContent() === productName)
+  if (await products.nth(i).locator("b").textContent() === productName)
   {
     await products.nth(i).locator("text=Add to cart").click();
     break;
   }
   }
+  await page.pause();
 });
