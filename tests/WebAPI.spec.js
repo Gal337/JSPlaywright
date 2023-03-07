42d22045f3b5a7a2ce9c692542fb8efc081e44c5
Gal Sesek <gal.sesek@endava.com>
2023-03-07 12:25:03 +0100

    Bypassing login and creating order with API call

--- tests/WebAPI.spec.js
+++ tests/WebAPI.spec.js
@@ -2,9 +2,11 @@ const {test, expect, request} = require('@playwright/test');
 
 //Creating variable with login payload for API endpoint as Java object {(property:"value")}
 const loginPayload = {userEmail:"4testing@gmail.com",userPassword:"Abc!2345"};
-
-//Making token variable accessable to every test
+//Creating Order paylod (same as above)
+const orderPayload = {orders: [{country: "Jamaica", productOrderedId: "6262e9d9e26b7e1a10e89c04"}]};
+//Making token and orderId variable accessable to every test
 let token;
+let orderId;
 
 //This block of code gets executed only once before all other tests
 test.beforeAll( async() =>
@@ -16,7 +18,7 @@ test.beforeAll( async() =>
   //Inside () we specify the URL, data we are sending {data:loginPayload},
   const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", 
   {
-    data:loginPayload
+    data: loginPayload
   })
   //Making assertion to check the call to API did not fail; we should recieve a code 2** (200 - OK)
   expect(loginResponse.ok()).toBeTruthy();
@@ -25,7 +27,18 @@ test.beforeAll( async() =>
   //Parsing JSON response body and extracting token from it
   token = loginResponseJson.token;
   console.log(token);
-
+  //Accessing API endpoint Create Order
+  const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+  {
+    data: orderPayload,
+    headers:{ 
+      'Authorization': token,
+      'Content-Type': 'application/json'
+      },
+  });
+  const orderResponseJson = await orderResponse.json();
+  console.log(orderResponseJson)
+  orderId = orderResponseJson.orders[0];
 });
 
 //This block of code gets executed each time before other tests
@@ -49,61 +62,10 @@ test('Web API test', async ({page}) =>
     window.localStorage.setItem('token', value);
   }, token);
 
-  const email = "4testing@gmail.com";
-  const productName = 'iphone 13 pro';
   const link = "https://rahulshettyacademy.com/client/";
   const products = page.locator(".card-body");
 
   await page.goto(link);
- 
-  const titles = await page.locator(".card-body b").allTextContents();
- 
-  console.log(titles);
- 
-  const count = await products.count();
-  for(let i = 0; i < count; ++i)
-  {
-  if (await products.nth(i).locator("b").textContent() === productName)
-  {
-    await products.nth(i).locator("text=Add to cart").click();
-    break;
-  }
-  }
- 
-  await page.locator("[routerlink*='cart']").click();
-  await page.locator("div li").first().waitFor()
- 
-  const bool = page.locator("h3:has-text('iphone 13 pro')").isVisible();
-  
-  expect(bool).toBeTruthy();
-
-  await page.locator("text=Checkout").click();
-  await page.locator("[placeholder*='Country']").type("can", {delay:100});
- 
-  const dropdown = page.locator(".ta-results");
-  
-  await dropdown.waitFor();
- 
-  const optionsCount = await dropdown.locator("button").count();
-
-  for (let i = 0; i < optionsCount; i++)
-  {
-    const text = await dropdown.locator("button").nth(i).textContent();
-    if (text === " Canada")
-    {
-
-      await dropdown.locator("button").nth(i).click();
-      break;
-    }
-  }
- 
-  await expect(page.locator(".user__name label[type='text']")).toHaveText(email);
-  await page.locator(".action__submit").click();
-  await expect(page.locator(".hero-primary")).toHaveText("Thankyou for the order.");
-
-  const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-  console.log(orderId);
- 
   await page.locator("button[routerlink*='/dashboard/myorders']").click();
   await page.locator("tbody").waitFor();
 
