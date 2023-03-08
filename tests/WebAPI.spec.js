93afbea866a708a427cb554dce4bb25b47f8b828
Gal Sesek <gal.sesek@endava.com>
2023-03-08 14:30:16 +0100

    Updated APIUtils file

--- tests/WebAPI.spec.js
+++ tests/WebAPI.spec.js
@@ -11,41 +11,13 @@ let orderId;
 //This block of code gets executed only once before all other tests
 test.beforeAll( async() =>
 {
-  //New API context needs to be created, so we can call the API
+  //New API context needs to be created, so the method that is in APIUtils file will run
   const apiContext = await request.newContext();
-  //Saving the API response to variable called loginResponse
-  //Calling the API and telling it POST method is being used
-  //Inside () we specify the URL, data we are sending {data:loginPayload},
-  const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", 
-  {
-    data: loginPayload
-  })
-  //Making assertion to check the call to API did not fail; we should recieve a code 2** (200 - OK)
-  expect(loginResponse.ok()).toBeTruthy();
-  //Getting the JSON response body and saving it to variable
-  const loginResponseJson = await loginResponse.json();
-  //Parsing JSON response body and extracting token from it
-  token = loginResponseJson.token;
-  console.log(token);
-  //Accessing API endpoint Create Order
-  const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
-  {
-    data: orderPayload,
-    headers:{ 
-      'Authorization': token,
-      'Content-Type': 'application/json'
-      },
-  });
-  const orderResponseJson = await orderResponse.json();
-  console.log(orderResponseJson)
-  orderId = orderResponseJson.orders[0];
 });
 
 //This block of code gets executed each time before other tests
 test.beforeEach(  () =>
 {
-  
-
 
 });
 
@@ -56,6 +28,9 @@ and search for Auto suggestive dropdown options & E2E flow test
  */
 test('Web API test', async ({page}) =>
 {
+  //
+  const apiUtils = new.apiUtils(apiContext, loginPayload);
+  const orderId = createOrder(orderPayload);
   //Adding initialization script to import the token
   page.addInitScript(value => {
     //Importing token
---
+++ tests/utils/APIUtils.js
@@ -0,0 +1,55 @@
+//Creating class called APIUtils
+class APIUtils
+{
+    
+    constructor(apiContext, loginPayload)
+    {
+      //Assigning api context from test case to local class api context
+      this.apiContext = apiContext;
+      this.loginPayload = loginPayload;
+    }
+
+    //Method for obtaining the token; async is used because the method includes await
+    async getToken()
+    {
+      //Calling the API and telling it POST method is being used
+      //Saving the API response to variable called loginResponse
+      //Inside () we specify the URL and the data we are sending {data:loginPayload}
+      const loginResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", 
+      {
+        data: this.loginPayload
+      })
+      //Making assertion to check the call to API did not fail; we should recieve a code 2** (200 - OK)
+      //Since this is only pre-condition data setup, assertion is not needed
+      //expect(loginResponse.ok()).toBeTruthy();
+      //Getting the JSON response body and saving it to variable
+      const loginResponseJson = await loginResponse.json();
+      //Parsing JSON response body and extracting token from it
+      token = loginResponseJson.token;
+      console.log(token);
+      //Method returns the token
+      return token;
+    }
+
+
+    //Method for creating order
+    async createOrder(orderPayload)
+    {
+      //Accessing API endpoint Create Order
+  const orderResponse = await this.apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+  {
+    data: orderPayload,
+    headers:{ 
+      'Authorization': this.getToken(),
+      'Content-Type': 'application/json'
+      },
+  });
+    const orderResponseJson = await orderResponse.json();
+    console.log(orderResponseJson)
+    orderId = orderResponseJson.orders[0];
+    return orderId;
+    }
+
+
+  
+}
