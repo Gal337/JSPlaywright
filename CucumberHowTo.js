c366de76c433bb43ced65744e2d259545ddbd497
Gal Sesek <gal.sesek@endava.com>
2023-05-30 11:44:57 +0200

    Modified CucumberHowTo file

--- CucumberHowTo.js
+++ CucumberHowTo.js
@@ -1,10 +1,11 @@
-/*One .feature file can hold multiple scenarios, which are treated
-as test cases. Keywords used are: Given, When, Then*/
-
-/*In one scenario, multiple keywords can be used.
-Given *i have info / data I need
-When *perform action*
-Then verify if performed action is correct*/
+/*RUNNING THE SCENARIOS*/
+/*Inside the project features folder should be created first!
+Example: *project name*, inside the project structure feature folder is created*/
+/*Inside feature folder, .feature file should exist;
+Example: features (folder), inside this folder new file is created called Ecommerce.feature*/
+/*Create folder with step definitions inside features folder;
+Example: features (folder), inside this folder another folder is created called step_definitions*/
+/*Run scenario by typing in terminal: npx cucumber-js*/
 
 
 /*.feature file should include the steps;
@@ -27,3 +28,13 @@ When('the greeter says hello', function () {
 Then('I should have heard {string}', function (expectedResponse) {
   assert.equal(this.whatIHeard, expectedResponse)
 });*/
+
+
+
+/*One .feature file can hold multiple scenarios, which are treated
+as test cases. Keywords used are: Given, When, Then*/
+
+/*In one scenario, multiple keywords can be used.
+Given *i have info / data I need
+When *perform action*
+Then verify if performed action is correct*/
---
+++ features/step_definitions/steps.js
@@ -0,0 +1,29 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+
+Given('user logs in to Ecommerce application with {username} and {password}', function (username, password) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('add {string} to cart', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('verify {string} is displayed in the cart', function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('enter valid details and place the order', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('verify order is present in OrderHistory', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+
+
+
+
